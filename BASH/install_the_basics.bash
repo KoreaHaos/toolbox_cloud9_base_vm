3df1439a4999023623eb4fefc92a0e09453ebddd
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-02-16 23:44:40 +0000

    NO COMMIT MESSAGE!. Script commit by : /home/ubuntu/haos_tools/BASH/github/add_commit_push_all.bash.

--- BASH/install_the_basics.bash
+++ BASH/install_the_basics.bash
@@ -3,5 +3,6 @@ printf "INSTALL THE BASICS!\n"
 function install_the_basics() {
     printf "Then let's INSTALL THE BASICS!\n";
     sleep 3;
-    git checkout 
+    git checkout the-basics
+    source ./basics-RUNME.bash;
 }
