2a59ff9c78f0e6ce4f3de1bc9f2238b1946038c9
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-02-17 04:41:12 +0000

    Maybe this time...

--- BASH/DELETE_EVERYTHING_BUT_C9_DIRECTORY.bash
+++ BASH/DELETE_EVERYTHING_BUT_C9_DIRECTORY.bash
@@ -1,1 +1,15 @@
-find . -not -regex "$1/.c9" -delete
+
+echo "Directory passed = $1";
+reg_exp=".$1/.c9*"
+echo "reg_exp = '$reg_exp'"
+
+find $1 -type f -not -path "$1/.c9*"
+
+#find . -regex "$reg_exp" -print
+# find . -regex "$reg_exp" -print
+# find . -not -regex "$reg_exp" -print
+
+echo "Press return to continue ...";
+read _pause;
+
+# find . -not -regex "$1/.c9" -delete
