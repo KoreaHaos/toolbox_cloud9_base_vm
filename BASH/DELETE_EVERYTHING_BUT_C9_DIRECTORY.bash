0ee1b1c599aa1c10827f38e67d4ae6eda89578e2
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-02-17 05:33:25 +0000

    OMG.

--- BASH/DELETE_EVERYTHING_BUT_C9_DIRECTORY.bash
+++ BASH/DELETE_EVERYTHING_BUT_C9_DIRECTORY.bash
@@ -1,6 +1,9 @@
-find $1 -maxdepth 1 -type f -delete
-find $1 -maxdepth 1 -not -name "*c9*" -type d -exec rm -rf {} +
+printf "These files and directories will be deleted :\n\n"
+find $1 -maxdepth 1 -type f -print
+find $1/ -maxdepth 1 -not -wholename "$1/.c9*" -not -wholename "$1/" -type d -print
 
 echo "Press return to continue ...";
 read _pause;
 
+find $1 -maxdepth 1 -type f -delete
+find $1/ -maxdepth 1 -not -wholename "$1/.c9*" -not -wholename "$1/" -type d -delete
